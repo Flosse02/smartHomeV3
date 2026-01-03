02bdf4a9f3f0fb04d01f5d55d812459f1236020b
Ewan <vandenator10@gmail.com>
2026-01-03 16:06:59 +0800

    Add jellyfin integration, currently no frontend

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, jsonify, send_from_directory
 from dotenv import load_dotenv
-import os
+import os, requests, subprocess, json
 
 load_dotenv()  
 app = Flask(__name__)
@@ -33,6 +33,53 @@ def slideshow_images():
 @app.route("/media/images/<path:filename>")
 def serve_image(filename):
     return send_from_directory(IMAGE_FOLDER, filename)
+
+# Get music code, this one is for jellyfin
+def loginAndGetMusic():
+    JELLYFIN_URL = os.getenv("JELLYFIN_URL")
+    JELLYFIN_API_KEY = os.getenv("JELLYFIN_API_KEY")
+    JELLYFIN_USER_ID = os.getenv("JELLYFIN_USER_ID")
+
+    response = requests.get(
+        f"{JELLYFIN_URL}/Users/{JELLYFIN_USER_ID}/Items",
+        params={
+            "IncludeItemTypes": "Audio",
+            "Recursive": "true",
+            "Fields": "Album,Artist,RunTimeTicks"
+        },
+        headers={"X-Emby-Token": JELLYFIN_API_KEY}
+    )
+    try:
+        data = response.json()
+    except Exception as e:
+        print("JSON PARSE ERROR:", e)
+        data = {"Items": []}  # fallback so Flask won't crash
+
+    return data.get("Items", [])
+
+@app.route("/api/music")
+def music():
+    musicDict = {}
+    music = loginAndGetMusic()
+
+    for item in music:
+        artist = item['Artists'][0] if item['Artists'] else "Unknown Artist"
+        album = item.get('Album', 'Unknown Album')
+        songData = {
+            "id": item['Id'],
+            "title": item['Name', 'Unknown Title'],
+            "duration": int(item['RunTimeTicks', 0] / 10_000_000)
+        }
+
+        if artist not in musicDict:
+            musicDict[artist] = {}
+        if album not in musicDict[artist]:
+            musicDict[artist][album] = []
+        musicDict[artist][album].append(songData)
+
+    return jsonify(musicDict)
+
+
 
 if __name__ == "__main__":
      app.run(host="127.0.0.1", port=5000, debug=True)
